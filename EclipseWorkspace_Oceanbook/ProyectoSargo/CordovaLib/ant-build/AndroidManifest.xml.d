04b110265ce56f8c275868f427ed859de583a2c0
aris.goico@gmail.com <aris.goico@gmail.com@48147b42-63ce-c86d-320a-5a5696d5e44d>
2014-08-23 17:31:55 +0000

    Nuevo workspace en el SVN

---
+++ EclipseWorkspace_Oceanbook/ProyectoSargo/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Sergio\Documents\Sargo\pruebas\phoneapupgrade\sargo\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Sergio\Documents\Sargo\pruebas\phoneapupgrade\sargo\platforms\android\CordovaLib\AndroidManifest.xml \
