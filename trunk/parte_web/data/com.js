638f0277f2ee136eba3606e0ad220d6a32bab9a7
aris.goico@gmail.com <aris.goico@gmail.com@48147b42-63ce-c86d-320a-5a5696d5e44d>
2014-09-06 15:20:19 +0000

    Version 0.3.1, arreglado un fallo editorial

---
+++ trunk/parte_web/data/com.js
@@ -0,0 +1,24 @@
+function fish ($scope){
+    $scope.data = [
+  {
+	"id": "0001",   
+	"mainimage": "img/main/001.jpg",
+	"general": "",
+	"description": "",
+	"commonspecies": "",
+	"profrange": "",
+	"sustrato": "",
+	"light": "",
+  },
+  /* {
+	"id": "0001",   
+	"mainimage": "img/main/001.jpg",
+	"general": "",
+	"description": "",
+	"commonspecies": "",
+	"profrange": "",
+	"sustrato": "",
+	"light": "",
+  },*/
+  ]
+  }
